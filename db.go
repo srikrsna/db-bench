3711e9b0ee1830dd751a2faa8aa2aba51e34533f
shan <shan.kulkarni.ts@gmail.com>
2018-04-17 14:05:11 +0530

    credentials json

--- db.go
+++ db.go
@@ -1,5 +1,12 @@
 package db
 
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"github.com/Jeffail/gabs"
+)
+
 // Store ...
 type Store interface {
 	Add(string, User) error
@@ -12,4 +19,19 @@ type User struct {
 	Name    string `datastore:"name"`
 	Address string `datastore:"address"`
 	Contact string `datastore:"contact"`
+}
+
+
+func getCredentials ()interface{} {
+	raw, err := ioutil.ReadFile("./credentials.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	settings, err := gabs.ParseJSON([]byte(raw))
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return settings
 }
--- db_test.go
+++ db_test.go
@@ -9,6 +9,7 @@ import (
 var user = db.User{Name:"test", Address: "India", Contact: "9988776651",}
 
 func BenchmarkNewInMemoryStore(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewInMemoryStore()
 	for i := 0; i < b.N; i++ {
 		store.Add("id", user)
@@ -16,6 +17,7 @@ func BenchmarkNewInMemoryStore(b *testing.B) {
 }
 
 func BenchmarkNewInMemoryStorePar(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewInMemoryStore()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
@@ -25,6 +27,7 @@ func BenchmarkNewInMemoryStorePar(b *testing.B) {
 }
 
 func BenchmarkNewDataStore(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewDataStore()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -34,53 +37,59 @@ func BenchmarkNewDataStore(b *testing.B) {
 
 
 func BenchmarkNewredis(b *testing.B) {
+	b.ReportAllocs()
 	store := db.Newredis()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		store.Add(fmt.Sprint("%d", i), user)
+		store.Add(fmt.Sprintf("%d", i), user)
 	}
 }
 
 func BenchmarkNewMongo(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewMongo()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		store.Add(fmt.Sprint("%d", i), user)
+		store.Add(fmt.Sprintf("%d", i), user)
 	}
 }
 
 
 func BenchmarkNewPostGres(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewPostGres()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		store.Add(fmt.Sprint("%d", i), user)
+		store.Add(fmt.Sprintf("%d", i), user)
 	}
 }
 
 
 func BenchmarkNewMySql(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewMySql()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		store.Add(fmt.Sprint("%d", i), user)
+		store.Add(fmt.Sprintf("%d", i), user)
 	}
 }
 
 func BenchmarkNewDynamoDB(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewDynamoDB()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		store.Add(fmt.Sprint("%d", i), user)
+		store.Add(fmt.Sprintf("%d", i), user)
 	}
 }
 
 
 func BenchmarkNewMemcached(b *testing.B) {
+	b.ReportAllocs()
 	store := db.NewMemcached()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		store.Add(fmt.Sprint("%d", i), user)
+		store.Add(fmt.Sprintf("%d", i), user)
 	}
 }
 
